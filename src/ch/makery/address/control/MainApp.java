2f06a76b0cf0ef56c68b3ff00d1f08a1a996a9f5
JoseManuel <josemanuelcrv@gmail.com>
2015-11-08 13:34:25 +0100

    Part 2. Add  Conexión de MainApp con PersonOverviewController Modificado método:  public void showPersonOverview() en Class MainApp

--- src/ch/makery/address/control/MainApp.java
+++ src/ch/makery/address/control/MainApp.java
@@ -4,6 +4,7 @@ package ch.makery.address.control;
 import java.io.IOException;
 
 import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -88,15 +89,23 @@ public class MainApp extends Application {
     /**
      * Shows the person overview inside the root layout.
      */
+    /**
+     * Shows the person overview inside the root layout.
+     */
     public void showPersonOverview() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
