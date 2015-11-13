fb22eb82c85816a4ecb4803378b230c94cf66921
JoseManuel <josemanuelcrv@gmail.com>
2015-11-13 18:53:51 +0100

    Corregida la ruta de carga de los XML. (../view/RootLayout.fxml)

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -8,56 +8,169 @@ import ch.makery.address.control.MainApp;
 import ch.makery.address.model.Person;
 
 public class PersonOverviewController {
-    @FXML
-    private TableView<Person> personTable;
-    @FXML
-    private TableColumn<Person, String> firstNameColumn;
-    @FXML
-    private TableColumn<Person, String> lastNameColumn;
+	@FXML
+	private TableView<Person> personTable;
+	@FXML
+	private TableColumn<Person, String> firstNameColumn;
+	@FXML
+	private TableColumn<Person, String> lastNameColumn;
 
-    @FXML
-    private Label firstNameLabel;
-    @FXML
-    private Label lastNameLabel;
-    @FXML
-    private Label streetLabel;
-    @FXML
-    private Label postalCodeLabel;
-    @FXML
-    private Label cityLabel;
-    @FXML
-    private Label birthdayLabel;
+	@FXML
+	private Label firstNameLabel;
+	@FXML
+	private Label lastNameLabel;
+	@FXML
+	private Label streetLabel;
+	@FXML
+	private Label postalCodeLabel;
+	@FXML
+	private Label cityLabel;
+	@FXML
+	private Label birthdayLabel;
 
-    // Reference to the main application.
-    private MainApp mainApp;
+	// Reference to the main application.
+	private MainApp mainApp;
 
-    /**
-     * The constructor.
-     * The constructor is called before the initialize() method.
-     */
-    public PersonOverviewController() {
-    }
+	/**
+	 * The constructor. The constructor is called before the initialize()
+	 * method.
+	 */
+	public PersonOverviewController() {
+	}
 
-    /**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     */
-    @FXML
-    private void initialize() {
-        // Initialize the person table with the two columns.
-        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
-    }
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		// Initialize the person table with the two columns.
+		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+	}
 
-    /**
-     * Is called by the main application to give a reference back to itself.
-     *
-     * @param mainApp
-     */
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 *
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
 
-        // Add observable list data to the table
-        personTable.setItems(mainApp.getPersonData());
-    }
+		// Add observable list data to the table
+		personTable.setItems(mainApp.getPersonData());
+	}
 }
+
+
+
+
+//package ch.makery.address.view;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TableColumn;
+//import javafx.scene.control.TableView;
+//import ch.makery.address.MainApp;
+//import ch.makery.address.model.Person;
+//import ch.makery.address.util.DateUtil;
+//
+//public class PersonOverviewController {
+//
+//	@FXML
+//	private TableView<Person> personTable;
+//	@FXML
+//	private TableColumn<Person, String> firstNameColumn;
+//	@FXML
+//	private TableColumn<Person, String> lastNameColumn;
+//	@FXML
+//	private Label firstNameLabel;
+//	@FXML
+//	private Label lastNameLabel;
+//	@FXML
+//	private Label streetLabel;
+//	@FXML
+//	private Label postalCodeLabel;
+//	@FXML
+//	private Label cityLabel;
+//	@FXML
+//	private Label birthdayLabel;
+//
+//	// Reference to the main application.
+//	private MainApp mainApp;
+//
+//	/**
+//	 * The constructor. The constructor is called before the initialize()
+//	 * method.
+//	 */
+//	public PersonOverviewController() {
+//	}
+//
+//	/**
+//	 * Initializes the controller class. This method is automatically called
+//	 * after the fxml file has been loaded.
+//	 */
+//	@FXML
+//	private void initialize() {
+//		// Initialize the person table with the two columns.
+//		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+//		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+//		// Clear person details.
+//		showPersonDetails(null);
+//
+//		// Listen for selection changes and show the person details when
+//		// changed.
+//		personTable.getSelectionModel().selectedItemProperty()
+//				.addListener((observable, oldValue, newValue) -> showPersonDetails(newValue));
+//	}
+//
+//	/**
+//	 * Is called by the main application to give a reference back to itself.
+//	 *
+//	 * @param mainApp
+//	 */
+//	public void setMainApp(MainApp mainApp) {
+//		this.mainApp = mainApp;
+//
+//		// Add observable list data to the table
+//		personTable.setItems(mainApp.getPersonData());
+//	}
+//
+//	/**
+//	 * Fills all text fields to show details about the person. If the specified
+//	 * person is null, all text fields are cleared.
+//	 *
+//	 * @param person
+//	 *            the person or null
+//	 */
+//
+//	@SuppressWarnings("unused")
+//	private void showPersonDetails(Person person) {
+//		if (person != null) {
+//			// Fill the labels with info from the person object.
+//			firstNameLabel.setText(person.getFirstName());
+//			lastNameLabel.setText(person.getLastName());
+//			streetLabel.setText(person.getStreet());
+//			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+//			cityLabel.setText(person.getCity());
+//			birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+//
+//			// TODO: We need a way to convert the birthday into a String!
+//			// birthdayLabel.setText(...);
+//		} else {
+//			// Person is null, remove all the text.
+//			firstNameLabel.setText("");
+//			lastNameLabel.setText("");
+//			streetLabel.setText("");
+//			postalCodeLabel.setText("");
+//			cityLabel.setText("");
+//			birthdayLabel.setText("");
+//		}
+//
+//	}
+//
+//	private void handleDeletePerson() {
+//		int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+//		personTable.getItems().remove(selectedIndex);
+//	}
+//}
