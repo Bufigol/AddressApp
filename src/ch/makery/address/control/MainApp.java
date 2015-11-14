26844adc89057acd29216238e5ab2fbb55acfab9
JoseManuel <josemanuelcrv@gmail.com>
2015-11-15 00:20:40 +0100

    Leyendo y escribiendo datos con JAXB:
    + Creados 2 metodos escritura (marshalling) y lectura (unmarshalling  en
    la clase MainApp;
    loadPersonDataFromFile(File file)
    savePersonDataToFile(File file)

--- src/ch/makery/address/control/MainApp.java
+++ src/ch/makery/address/control/MainApp.java
@@ -4,7 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.prefs.Preferences;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.controlsfx.dialog.Dialogs;
+
 import ch.makery.address.model.Person;
+import ch.makery.address.model.PersonListWrapper;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
@@ -192,6 +199,67 @@ public class MainApp extends Application {
 	        primaryStage.setTitle("AddressApp");
 	    }
 	}
+
+
+	/**
+	 * Loads person data from the specified file. The current person data will
+	 * be replaced.
+	 *
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file) {
+	    try {
+	        JAXBContext context = JAXBContext
+	                .newInstance(PersonListWrapper.class);
+	        Unmarshaller um = context.createUnmarshaller();
+
+	        // Reading XML from the file and unmarshalling.
+	        PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+	        personData.clear();
+	        personData.addAll(wrapper.getPersons());
+
+	        // Save the file path to the registry.
+	        setPersonFilePath(file);
+
+	    } catch (Exception e) { // catches ANY exception
+	        Dialogs.create()
+	                .title("Error")
+	                .masthead("Could not load data from file:\n" + file.getPath())
+	                .showException(e);
+	    }
+	}
+
+	/**
+	 * Saves the current person data to the specified file.
+	 *
+	 * @param file
+	 */
+	public void savePersonDataToFile(File file) {
+	    try {
+	        JAXBContext context = JAXBContext
+	                .newInstance(PersonListWrapper.class);
+	        Marshaller m = context.createMarshaller();
+	        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+	        // Wrapping our person data.
+	        PersonListWrapper wrapper = new PersonListWrapper();
+	        wrapper.setPersons(personData);
+
+	        // Marshalling and saving XML to the file.
+	        m.marshal(wrapper, file);
+
+	        // Save the file path to the registry.
+	        setPersonFilePath(file);
+	    } catch (Exception e) { // catches ANY exception
+	        Dialogs.create().title("Error")
+	                .masthead("Could not save data to file:\n" + file.getPath())
+	                .showException(e);
+	    }
+	}
+
+
+
 
 
 
